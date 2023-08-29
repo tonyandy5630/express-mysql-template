5ec56f510cd54068bcfb332f6a427d7e6b4b4685
tonyandy5630 <tonyandy456@gmail.com>
2023-08-29 23:02:30 +0700

    change default files

---
+++ src/database/config/config.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    logging: false,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  },
+  production: {
+    use_env_variable: "ENTER_DB_NAME",
+    dialect: "mysql",
+    dialectModule: require("mysql2"),
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,47 @@
 const express = require("express");
-const dotenv = require("dotenv");
-
-dotenv.config();
-
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const router = require("./routes");
 const app = express();
-const port = process.env.PORT;
+const path = require("path");
+const db = require("./database/models");
+app.use(express.static(path.join(__dirname, "dist")));
+require("dotenv").config();
+const port = process.env.NODE_ENV === undefined ? 8080 : process.env.PORT;
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+const whitelist = [
+  process.env.CLIENT_URL_PRODUCTION,
+  process.env.CLIENT_URL_DEVELOPMENT,
+  process.env.CLIENT_URL_DOMAIN,
+];
+
+app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS at " + origin));
+      }
+    }, // allow to server to accept request from different origin
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true, // allow session cookie from browser to pass through
+    allowedHeaders: "Origin,X-Requested-With,Content-Type,Accept",
+  })
+);
+
+app.use(router);
+db.sequelize
+  .sync()
+  .then(() => {
+    console.log("Synced db.");
+  })
+  .catch((err) => {
+    console.log("Failed to sync db: " + err.message);
+  });
+
+app.listen(port, "0.0.0.0", (err) => {
+  if (err) console.log(err);
+  console.log("running on port: " + port);
 });
